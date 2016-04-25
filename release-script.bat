6fdd1386fa5f27d5f6c3ed15fa99f915201b898b
radmilabord <r.bord@elsevier.com>
2016-04-24 20:03:27 -0400

    t3

--- release-script.bat
+++ release-script.bat
@@ -1,22 +1,4 @@
-
 REM update all dependencies to the latest released version
-mvn versions:use-latest-versions -Dincludes=com.rada:* 
-
-REM This will strip off SNAPSHOT from the project version
-mvn -B build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}
-
-REM deploy artifact - this might not be needed if next step is to commit released version. Depends if branches are automatically built. If they are, then deploying is not needed. If they don't and release happens in the release branch, then this step is needed
-mvn deploy
-
-REM commit released version
-git add .
-git commit -m "Commiting released version"
-git push
-
-REM update project version to the next snapshot
-mvn -B release:update-versions
-
-REM commit released version
-git add .
-git commit -m "Commiting new snapshot version"
-git push
+call mvn versions:use-latest-versions -Dincludes=com.rada:* 
+call mvn release:prepare -Dusername=radmilabord -Dpassword=pinelopa80
+call mvn release:perform
