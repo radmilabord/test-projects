908a8726cab4eae50ade12e6c98c4e4369a5c4c5
radmilabord <r.bord@elsevier.com>
2016-04-24 20:11:03 -0400

    p

--- release-script.bat
+++
@@ -1,4 +0,0 @@
-REM update all dependencies to the latest released version
-call mvn versions:use-latest-versions -Dincludes=com.rada:* 
-call mvn release:prepare -Dusername=radmilabord -Dpassword=pinelopa80
-call mvn release:perform
